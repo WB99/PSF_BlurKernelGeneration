d36b092833e54950aed4faa7c371d48c57443390
Wong Wei Bin <wong1231@e.ntu.edu.sg>
2024-03-07 11:44:12 +0800

    changed blurkernelgeneration to generate rgb kernels instead of grayscale, added subdirectory to save kernels

--- blurKernelGeneration.m
+++ blurKernelGeneration.m
@@ -1,3 +1,11 @@
+% Define the subdirectory name
+subdir_name = 'blur_kernels';
+
+% Check if the subdirectory exists, if not, create it
+if ~exist(subdir_name, 'dir')
+    mkdir(subdir_name);
+end
+
 % Define the anxiety (A) and exposure (E) levels
 A_levels = [0.005, 0.001, 0.00005];
 E_levels = [1/25, 1/10, 1/5];
@@ -23,10 +31,20 @@ for i = 1:length(A_levels)
         PSF = createPSFs(Trajectory, PSFsize, E_levels(j), do_show, do_center); % Include other necessary parameters
         PSF_matrix = PSF{1}; % Access the first element since there is only 1 PSF per cell
         
-        % Make sure the PSF is normalized and convert it to uint8
-        PSF_matrix = uint8(255 * mat2gray(PSF_matrix));
+        % Normalize the PSF to the range [0, 1]
+        PSF_normalized = mat2gray(PSF_matrix);
+        
+        % Replicate the grayscale PSF across three channels to create an RGB image
+        PSF_rgb = repmat(PSF_normalized, [1 1 3]);
 
-        % Save the centered PSF as a JPG image
-        imwrite(PSF_matrix, sprintf('kernel_A%d_E%d.jpg', i, j));
+        % Convert to uint8
+        PSF_uint8 = im2uint8(PSF_rgb);
+
+        % Construct the filename with the subdirectory
+        filename = fullfile(subdir_name, sprintf('kernel_A%d_E%d.jpg', i, j));
+
+        % Write the file to the subdirectory
+        imwrite(PSF_uint8, filename);
+      
     end
 end
