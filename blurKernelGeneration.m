bf40f2ff96cb55f37c4cb773d07b28513cd2a209
Wong Wei Bin <wong1231@e.ntu.edu.sg>
2024-03-12 22:14:46 +0800

    added loop to generate subfolders and set number of kernels generated

--- blurKernelGeneration.m
+++ blurKernelGeneration.m
@@ -1,10 +1,20 @@
 % Check if the subdirectory exists, if not, create it
-if ~exist('blur_kernels', 'dir')
-    mkdir('blur_kernels');
+blurLevels = {'l_1', 'l_2', 'l_3'};
+baseDir = 'blur_kernels_levelwise';
+
+if ~exist(baseDir, 'dir')
+    mkdir(baseDir);
+end
+
+for b = 1:length(blurLevels)
+    levelDir = fullfile(baseDir, blurLevels{b});
+    if ~exist(levelDir, 'dir')
+        mkdir(levelDir);
+    end
 end
 
 % Define the anxiety (A) and exposure (E) levels
-A_levels = [0.005, 0.003, 0.001];
+A_levels = [0.005, 0.001, 0.00005];
 E_levels = [1/25, 1/10, 1/5];
 
 % Trajectory curve parameters
@@ -13,33 +23,30 @@ numT = 2000;
 MaxTotalLength = 64;
 do_show = 0;
 do_center = 1;
+numKernelsPerLevel = 100; % Number of kernels to generate per A&E combination
 
+% Generate and save the kernels
+for a = 1:length(A_levels)
+    for e = 1:length(E_levels)
+        for k = 1:numKernelsPerLevel
+            % Generate the trajectory
+            Trajectory = createTrajectory(PSFsize, A_levels(a), numT, MaxTotalLength, do_show);
 
-% Iterate over each combination of A and E levels
-for i = 1:length(A_levels)
-    for j = 1:length(E_levels)
-        % Generate the trajectory using the createTrajectory function
-        Trajectory = createTrajectory(PSFsize, A_levels(i), numT, MaxTotalLength, do_show); % Include other necessary parameters
+            % Generate the PSF
+            PSF = createPSFs(Trajectory, PSFsize, E_levels(e), do_show, do_center);
+            PSF_matrix = PSF{1}; % Assuming PSF is a cell array
 
-        % Generate the PSF using the createPSFs function
-        PSF = createPSFs(Trajectory, PSFsize, E_levels(j), do_show, do_center); % Include other necessary parameters
-        PSF_matrix = PSF{1}; % Access the first element since there is only 1 PSF per cell
-        
-        % Convert PSF to RGB
-        % Normalize the PSF to the range [0, 1]
-        PSF_normalized = mat2gray(PSF_matrix);
-        
-        % Replicate the grayscale PSF across three channels to create an RGB image
-        PSF_rgb = repmat(PSF_normalized, [1 1 3]);
+            % Convert PSF to RGB
+            PSF_normalized = mat2gray(PSF_matrix);
+            PSF_rgb = repmat(PSF_normalized, [1 1 3]);
+            PSF_uint8_rgb = im2uint8(PSF_rgb);
 
-        % Convert to uint8
-        PSF_uint8_rgb = im2uint8(PSF_rgb);
+            % Construct the directory and filename
+            subdir = fullfile(baseDir, ['l_', num2str(e)]);
+            filename = fullfile(subdir, sprintf('kernel_A%d_E%d_%d.jpg', a, e, k));
 
-        % Construct the filename with the subdirectory
-        filename = fullfile('blur_kernels', sprintf('kernel_A%d_E%d.jpg', i, j));
-
-        % Write the file to the subdirectory
-        imwrite(PSF_uint8_rgb, filename);
-      
+            % Write the kernel image to the file
+            imwrite(PSF_uint8_rgb, filename);
+        end
     end
 end
